dda7d0d597d0bebdb47c18e729095c39bae78177
Rafa-lopez12 <raf.asd09.lopez@gmail.com>
2025-11-27 23:23:09 -0400

    despliegue5

--- resources/js/composables/useApi.js
+++ resources/js/composables/useApi.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { ref } from 'vue';
 
 // Config base axios
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = 'api';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
